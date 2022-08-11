8f87ae8857494ab82e56090880f6a4a68fe179c9
Duffey_kay <kayodesunday29@gmail.com>
2022-08-11 14:55:21 +0100

    updated readme file

--- src/components/popularpage/popular.jsx
+++ src/components/popularpage/popular.jsx
@@ -11,7 +11,7 @@ const Popular=()=> {
 
   useEffect(() => {
     getPopular();
-  }, []);
+  }, [popular]);
 
   const getPopular = async () => {
     setStore(() => localStorage.getItem("popular"));
